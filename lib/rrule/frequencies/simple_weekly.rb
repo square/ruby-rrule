83ca3ed058fbe94e6680e3c28184b3746627569c
Thomas Cannon <tcannon00@gmail.com>
2018-05-09 13:38:13 -0400

    Set up SimpleWeekly to correct current_date to be at target weekday
    * If there is no `byweekday` option, the target weekday is the weekday
      of the `context.dtstart`
      * Otherwise, it's the `byweekday` option.
      * When we're finding the next occurences, we should correct the
        current date to be for the target weekday

--- lib/rrule/frequencies/simple_weekly.rb
+++ lib/rrule/frequencies/simple_weekly.rb
@@ -1,9 +1,22 @@
 module RRule
   class SimpleWeekly < Frequency
     def next_occurrences
+      correct_current_date_if_needed
       this_occurrence = current_date
       @current_date += context.options[:interval].weeks
       [this_occurrence]
+    end
+
+    def correct_current_date_if_needed
+      if context.options[:byweekday].present?
+        target_wday = context.options[:byweekday].first.index
+      else
+        target_wday = context.dtstart.wday
+      end
+
+      while @current_date.wday != target_wday
+        @current_date = @current_date + 1.day
+      end
     end
   end
 end
